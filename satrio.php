5fe8944cb31fcb060dee3cc3e19d04a24637f797
SatrioAugist <110141508+SatrioAugist@users.noreply.github.com>
2022-08-23 15:02:32 +0700

    Add files via upload

---
+++ satrio.php
@@ -0,0 +1,61 @@
+<?php
+    echo"
+        <marquee>
+            <h1>Satrio Augistiawan-Baru sekolah </h1>
+        </marquee>
+        
+        ";
+
+        //Variable
+
+        $nama = "Satrio Augist";
+        $kelas = "XI RPL 2";
+        $nilai = 89.69;
+
+
+        echo "Nama : ". $nama;
+            echo "<br>";
+        echo "Kelas : ". $kelas;
+            echo "<br>";
+        echo "Nilai : $nilai";
+            echo "<br>";
+            echo "<br>";
+        
+        //Operator Jumlah
+        $angka1 = 5;
+        $angka2 = 10;
+
+        $hasil = $angka1 + $angka2;
+        echo "Hasil dari penjumlahan : $hasil";
+            echo "<br>";
+            echo "<br>";
+
+        //Rumus luas segitiga
+         $alas = 8;
+         $tinggi = 20;
+
+         $rumus = 0.5 * $alas * $tinggi;
+         echo "Hasil rumus : $rumus";
+            echo "<br>";
+            echo "<br>";
+
+         //Rumus luas lingkaran
+    
+         $phi = 3.14;
+         $r = 7;
+
+         $lingkaran = $phi * $r * $r;
+         echo "Hasil luas lingkaran : $lingkaran";
+            echo "<br>";
+            echo "<br>";
+        
+        //Rumus volume balok
+
+        $p = 10;
+        $l = 5;
+        $t = 10;
+
+        $balok = $p * $l * $t;
+        echo "Hasil Volume Balok : $balok";
+    
+    ?>
